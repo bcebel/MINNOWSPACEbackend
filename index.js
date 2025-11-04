d9be9ef2affa50066a5bf676fcbc5b9e896e7625
bcebel <bcebel@gmail.com>
2025-11-03 21:08:43 -0600

    cors

--- index.js
+++ index.js
@@ -47,6 +47,14 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+// Step 3: Set up Apollo Server
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await apolloServer.start();
+apolloServer.applyMiddleware({ app, path: "/graphql", cors: false });
 app.get("/api/health", (req, res) => {
   res.json({ status: "OK", timestamp: new Date().toISOString() });
 });
@@ -221,13 +229,7 @@ const fetchUserAffiliateData = async (userId) => {
 // Then in your VideoCard component, you can fetch affiliate data when needed
 
 videoUploadHandler(app);
-// Step 3: Set up Apollo Server
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
-await apolloServer.start();
-apolloServer.applyMiddleware({ app, path: "/graphql", cors: false });
+
 // Step 8: Start the Server
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
