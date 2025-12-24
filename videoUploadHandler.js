a0f9234cd3a25f354d452e6e5ff8beda51f1acb2
bcebel <bcebel@gmail.com>
2025-12-23 19:45:54 -0600

    things

--- videoUploadHandler.js
+++ videoUploadHandler.js
@@ -125,7 +125,8 @@ async function handleUpload(req, res) {
     };
 
     await s3.send(new PutObjectCommand(params));
-const ipfsUrl = `https://ipfs.filebase.io/ipfs/${cid}`;
+const ipfsUrl = `https://${PINATA_GATEWAY}/ipfs/${cid}`;
+
     // Determine file type
     const fileType = getFileType(req.file.mimetype, req.file.originalname);
     const isImage = fileType === "image";
