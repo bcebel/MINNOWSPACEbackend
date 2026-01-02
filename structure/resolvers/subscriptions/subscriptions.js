9e5157bbc94fb4fb91249fe28c55b210f0bbf905
bcebel <bcebel@gmail.com>
2026-01-01 22:50:25 -0600

    scope

--- structure/resolvers/subscriptions/subscriptions.js
+++ structure/resolvers/subscriptions/subscriptions.js
@@ -1,25 +1,26 @@
-import { withFilter } from 'graphql-subscriptions';
-import { pubsub } from "../../pubsub.js";
+import { withFilter } from "graphql-subscriptions";
+import { pubsub } from "../../pubsub.js"; // <--- This is the one we want!
 
 const resolvers = {
   Subscription: {
     messageAdded: {
-      subscribe: (_, { chatId }, { pubsub }) => {
+      // Remove { pubsub } from the 3rd argument
+      subscribe: (_, { chatId }) => {
         return pubsub.asyncIterator(`MESSAGE_ADDED_${chatId}`);
       },
     },
     postAdded: {
-      subscribe: (_, { feedType, groupId }, { pubsub }) => {
+      // Remove { pubsub } from the 3rd argument
+      subscribe: (_, { feedType, groupId }) => {
         if (feedType) return pubsub.asyncIterator(`POST_ADDED_${feedType}`);
         if (groupId) return pubsub.asyncIterator(`POST_ADDED_GROUP_${groupId}`);
         return pubsub.asyncIterator("POST_ADDED_UNIVERSAL");
       },
     },
     livestreamChunkAdded: {
-      subscribe: (_, { sessionId }, { pubsub }) => {
-        // 1. Validation check
-        if (!pubsub) throw new Error("PubSub instance missing in context");
-
+      // Remove { pubsub } from the 3rd argument
+      subscribe: (_, { sessionId }) => {
+        // Now 'pubsub' refers to your import at the top
         const topic = `LIVESTREAM_CHUNK_ADDED_${sessionId}`;
         console.log(`📡 [SUBSCRIPTION] User started listening to: ${topic}`);
 
