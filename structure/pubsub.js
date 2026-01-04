fd5e1a1ae47a80cd44168305ba937516b5a276cd
bcebel <bcebel@gmail.com>
2026-01-03 18:53:46 -0600

    things

--- structure/pubsub.js
+++ structure/pubsub.js
@@ -1,11 +1,2 @@
-// structure/pubsub.js
 import { PubSub } from "graphql-subscriptions";
-
-// Create a single instance
-const pubsub = new PubSub();
-
-console.log("📡 PubSub instance created at startup");
-
-// Export both named and default
-export { pubsub };
-export default pubsub;
+export const pubsub = new PubSub();
