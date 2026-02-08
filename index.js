d00df323e09245f930b0cf20c69f98b4fd1b0d3e
bcebel <bcebel@gmail.com>
2026-02-08 15:13:07 -0600

    things

--- index.js
+++ index.js
@@ -330,7 +330,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 50 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
