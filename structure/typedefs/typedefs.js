64bb463ea411d39b6b035dd3e220ab609f950346
bcebel <bcebel@gmail.com>
2026-08-07 22:42:28 -0500

    things

--- structure/typedefs/typedefs.js
+++ structure/typedefs/typedefs.js
@@ -304,7 +304,7 @@ const typeDefs = gql`
     message(id: ID!): Message
 
     # Post queries
-    posts(neighborhoodId: ID): [Post!]!
+    posts(neighborhoodId: ID, limit: Int, offset: Int): [Post!]!
     post(id: ID!): Post
 
     # Group queries
